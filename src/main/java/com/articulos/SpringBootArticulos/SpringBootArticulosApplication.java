84eb3f6b3ca73d19e81366a99bceac728cdb1595
axels18m <axels18m@gmail.com>
2020-11-24 17:26:17 -0600

    Import project

---
+++ src/main/java/com/articulos/SpringBootArticulos/SpringBootArticulosApplication.java
@@ -0,0 +1,13 @@
+package com.articulos.SpringBootArticulos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootArticulosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootArticulosApplication.class, args);
+	}
+
+}
